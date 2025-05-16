ae79163a89d8128d89bbc3ea843285a71530fca4
JwpAT <150574239+JwpAT@users.noreply.github.com>
2025-05-16 19:39:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,15 +15,15 @@ echo "installing packages"
 yay -S --needed - < packages
 
 echo "Extracting Kripton theme..."
-tar -xf ~/hypr/Kripton.tar.xz
-mv ~/hypr/Kripton ~/.themes/
+tar -xf ~/hypr-old/Kripton.tar.xz
+mv ~/hypr-old/Kripton ~/.themes/
 
 echo "Kripton theme has been installed successfully."
 
 curl -fsSL https://ollama.com/install.sh | sh
 
 echo "Installing config files"
-mv ~/hypr/config/* ~/.config/
+mv ~/hypr-old/config/* ~/.config/
 
 echo "Installation has completed. Reboot your fucking system. Have fun!!"
 
